125bb2ebcecf536d813f0af948ed2fb2f0c063cd
unknown <user@nvist233.zyfra.com>
2023-10-01 17:52:56 +0300

    Версия продукта №1 покачто умеет только объединять два PDF файла в один, имена файлов указываются в коде

---
+++ main.py
@@ -0,0 +1,18 @@
+import PyPDF2
+
+# Список имен файлов, которые нужно объединить
+pdf_files = ["7721.pdf", "7722.pdf"]
+
+# Создайте объект PdfMerger
+pdf_merger = PyPDF2.PdfMerger()
+
+# Объедините файлы
+for pdf_file in pdf_files:
+    pdf_merger.append(pdf_file)
+
+# Сохраните объединенный документ
+with open("merged.pdf", "wb") as output_pdf:
+    pdf_merger.write(output_pdf)
+
+# Закройте объект PdfFileMerger
+pdf_merger.close()
